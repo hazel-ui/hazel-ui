4ea56d85698fb1bf7ea239e090404a418eb8423d
cseas <contact.abhijeetsingh@gmail.com>
2020-10-06 14:04:40 +0530

    Updates

---
+++ precache-manifest.44d6b65c158029042d6451a66e095fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3fa13e4e4c88c80a6cc03ef8c9e578",
+    "url": "/react-table-custom/index.html"
+  },
+  {
+    "revision": "ee20f63d8eb406a194cd",
+    "url": "/react-table-custom/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b64f4cd411b030669032",
+    "url": "/react-table-custom/static/js/2.aa6fcb6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-table-custom/static/js/2.aa6fcb6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee20f63d8eb406a194cd",
+    "url": "/react-table-custom/static/js/main.e13c0d92.chunk.js"
+  },
+  {
+    "revision": "70c5d7ef9ad8447c62f7",
+    "url": "/react-table-custom/static/js/runtime-main.4d81aeac.js"
+  }
+]);
