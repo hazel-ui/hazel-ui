5b7a1b6f8847dd93ccda5a140c8c527ae397e04b
cseas <contact.abhijeetsingh@gmail.com>
2020-10-18 10:59:59 +0530

    Updates

--- precache-manifest.7c37cebef057b8435fe03cec9504f0ea.js
+++ precache-manifest.7c37cebef057b8435fe03cec9504f0ea.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8de7886b658f7644146d12f6b87ba6eb",
+    "revision": "e9a5eae9afd9aaf71748f665c6f3fadc",
     "url": "/react-table-custom/index.html"
   },
   {
-    "revision": "91ad9c20b72b1eba1b5d",
+    "revision": "c35e95c14efc0f8949fd",
     "url": "/react-table-custom/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "6192d12ec47776f02cc8",
-    "url": "/react-table-custom/static/js/2.b1e5830b.chunk.js"
+    "revision": "88811f7b9d9459221ab4",
+    "url": "/react-table-custom/static/js/2.6e07b34e.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-table-custom/static/js/2.b1e5830b.chunk.js.LICENSE.txt"
+    "url": "/react-table-custom/static/js/2.6e07b34e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "91ad9c20b72b1eba1b5d",
-    "url": "/react-table-custom/static/js/main.b1cd64a7.chunk.js"
+    "revision": "c35e95c14efc0f8949fd",
+    "url": "/react-table-custom/static/js/main.c732adb3.chunk.js"
   },
   {
     "revision": "70c5d7ef9ad8447c62f7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-table-custom/precache-manifest.effc3e2feb3827129ec7ddd12149bda8.js"
+  "/react-table-custom/precache-manifest.7c37cebef057b8435fe03cec9504f0ea.js"
 );
 
 self.addEventListener('message', (event) => {
