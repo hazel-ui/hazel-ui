368f55bf9e67b06cf147bf9a5e5b53ed80fdfb35
cseas <contact.abhijeetsingh@gmail.com>
2020-10-26 20:25:45 +0530

    Add license & repository

---
+++ src/foundation/Icon/Icon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Icon() {
+  return <>{"  ↓"}</>;
+}
+/*
+    Static properties for the Icon component.
+    String icon can be replaced by svg elements.
+*/
+Icon.Sort = "  ⮃";
+Icon.ChironUp = "  ↑";
+Icon.ChironDown = "  ↓";
