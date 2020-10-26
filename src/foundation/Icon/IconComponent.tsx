6de8d0b26a515a57e2470039ad74c13f92918656
mahesh <jmahesh123@users.noreply.github.com>
2020-10-26 05:57:37 +0530

    Add Icon component with icon names as static properties (#17)
    Co-authored-by: Mahesh.Jawadi <Mahesh.Jawadi@externals.hilti.com>

---
+++ src/foundation/Icon/IconComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const IconComponent = () => {
+    return (
+        <>
+        {"  ↓"}
+        </>
+    )
+}
+/*
+    Static properties for the Icon component.
+    String icon can be replaced by svg elements.
+*/
+IconComponent.Sort =  "  ⮃";
+IconComponent.ChironUp = "  ↑";
+IconComponent.ChironDown  = "  ↓"
+export default IconComponent;
