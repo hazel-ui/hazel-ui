9dd6ae8e7866f0a8ea1e2989e15255d7a2480838
Mahesh.Jawadi <Mahesh.Jawadi@externals.hilti.com>
2020-10-28 15:32:18 +0530

    fixed test case

--- src/components/Badge/__tests__/Badge.test.tsx
+++ src/components/Badge/__tests__/Badge.test.tsx
@@ -6,7 +6,6 @@ import { Badge } from "../Badge";
 describe("<Badge>", () => {
   test("Renders Badge tag with children text", () => {
     render(<Badge>some text</Badge>);
-    expect(screen.getByRole("Badge")).toBeInTheDocument();
     expect(screen.getByText(/some text/i)).toBeInTheDocument();
   });
 });
