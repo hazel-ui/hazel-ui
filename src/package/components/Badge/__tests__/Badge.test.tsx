9ae3558e6c9076664c768ae9a829f1704079cb29
cseas <contact.abhijeetsingh@gmail.com>
2020-11-04 20:28:48 +0530

    Badge: Fix failing test

--- src/package/components/Badge/__tests__/Badge.test.tsx
+++ src/package/components/Badge/__tests__/Badge.test.tsx
@@ -1,4 +1,5 @@
 import { screen } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
 import React from "react";
 import { Badge } from "../Badge";
