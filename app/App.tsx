a7f6db7fbbe21994e9048fc618495fd24442dae2
Abhijeet Singh <contact.abhijeetsingh@gmail.com>
2021-07-26 21:42:59 +0530

    Setup eslint (#113)

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,7 @@
 import { Box, Button } from "../src/package";
 // import { Box, Slider } from "../dist";
 
-export function App() {
+export function App(): JSX.Element {
   return (
     <Box margin="50px">
       <Button>Click me</Button>
