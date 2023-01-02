bfdf022c734b4acd3714641a28a98a1881a33e16
Abhijeet Singh <contact.abhijeetsingh@gmail.com>
2023-01-02 21:21:28 +0530

    fix: tailwind not working in prod build (#157)
    * fix: tailwind not working in prod build
    * lint: ignore config files

--- webpack.prod.mjs
+++ webpack.prod.mjs
@@ -6,27 +6,6 @@ export default {
   ...baseConfig,
   entry: "./app/index.tsx",
   mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./app/index.html",
