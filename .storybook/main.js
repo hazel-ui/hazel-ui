14f41ceaa6d5c65ed1fc75270d78ed66d2f0dbcc
Abhijeet Singh <abhijeet.singh@razorpay.com>
2023-01-08 17:02:32 +0530

    chore: upgrade storybook to 7-beta for esm support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,14 @@
-module.exports = {
+export default {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   core: {
-    builder: "webpack5",
-    disableTelemetry: true, // https://storybook.js.org/docs/react/configure/telemetry#how-to-opt-out
+    disableTelemetry: true,
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };
