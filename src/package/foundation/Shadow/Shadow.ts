12f57700974f835951f98076d026d89e0f71b502
Abhijeet Singh <abhijeet.singh@razorpay.com>
2023-01-11 09:16:16 +0530

    chore: remove styled-system from Card

--- src/package/foundation/Shadow/Shadow.ts
+++ src/package/foundation/Shadow/Shadow.ts
@@ -1,6 +1,8 @@
 export enum Shadow {
   xs = "0 0px 6px 0 rgba(0, 0, 0, 0.15)",
   s = "0 3px 7px 0 rgba(0, 0, 0, 0.1)",
-  m = "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+  // TODO: Replace the color with Color enum when TypeScript 5 is out:
+  // https://github.com/microsoft/TypeScript/pull/50528#issuecomment-1322496801
+  m = "0 4px 8px 0 var(--gray7)",
   l = "0 8px 16px 0 rgba(0, 0, 0, 0.2)",
 }
