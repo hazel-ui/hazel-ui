447e6aa063eaebe6ed581606a73d7995ce31f4a9
Abhijeet Singh <abhijeet.singh@razorpay.com>
2023-01-14 20:02:57 +0530

    fix: tree shaking by adding sideEffects field

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,4 @@
-import { Typography } from "../dist/foundation/Typography/Typography.js";
-import { Color } from "../dist/foundation/Color/Color.js";
+import { Typography, Color } from "../dist/index.js";
 // import { Typography, Color } from "../src/package/index.js";
 
 export function App(): JSX.Element {
